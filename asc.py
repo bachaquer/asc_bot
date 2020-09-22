0e0832af52c5ab421470bd555d3748408e52d865
Mukhlis Batyrkhan <62286980+bachaquer@users.noreply.github.com>
2020-09-22 21:02:16 +0500

    Add files via upload

---
+++ asc.py
@@ -0,0 +1,16 @@
+from telegram.ext import Updater
+updater = Updater(token='1222720388:AAFAENX9hOJUud2jobUHypmOBpVqIuiu6zU', use_context=True)
+
+dispatcher = updater.dispatcher
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+from telegram.ext import CommandHandler
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+    
